8d739d4133c5e33445be680af0b76721b5a1d583
Casper van Elteren <Casper van Elteren>
2016-12-17 13:21:26 +0100

    update

--- Assignment 4/Code/Ex1.py
+++ Assignment 4/Code/Ex1.py
@@ -3,6 +3,7 @@ from scipy.stats import multivariate_normal as mv
 from pylab import *
 
 
+# q1
 def kernel_function(x, theta):
     '''
     Code for equation 3
@@ -17,15 +18,16 @@ def kernel_function(x, theta):
     return sigma
 
 
-
+# q2
 # number of points to sample
 N = 101
 x = np.linspace(-1, 1, N)
 theta = [1,1,1,1]
 K = kernel_function(x, theta)
 
+# q4
 mu = np.zeros(len(x))
-# for some reason it erros on singularity?
+# for some reason it errors on singularity?
 prior = mv(mu, K, allow_singular = 1)
 samples = prior.rvs(5)
 
@@ -33,4 +35,28 @@ fig, ax = subplots(1,1)
 for i in samples:
     ax.plot(x, i)
 savefig('../Figures/ex_1_test')
+
+thetas = np.array([\
+[1, 4, 0, 0], \
+[9, 4, 0, 0],\
+[1, 64, 0, 0],\
+[1, 0.25, 0,0],\
+[1, 4, 10, 0],\
+[1, 4, 0, 5] ])
+
+# i want at most 3 rows
+nRows = 3
+# get the the number of columns
+nCols = thetas.shape[0] // nRows
+
+fig, ax = subplots(nrows = nRows, ncols = nCols, sharex = 'all')
+for idx, theta in enumerate(thetas):
+    K = kernel_function(x, theta)
+    prior = mv(mu, K, allow_singular = 1)
+    samples = prior.rvs(5)
+    for i in samples:
+        ax.flatten()[idx].plot(x, i)
+    ax.flatten()[idx].set_title(theta)
+fig.tight_layout()
+savefig('../Figures/ex1_test2.png')
 show(block = 1)
