d54b0ef29b700e9498fdaf83bc5be19b4eb409aa
Casper van Elteren <Casper van Elteren>
2016-12-28 14:32:41 +0100

    full assignment minus bonus

--- Assignment 4/Code/ex2.py
+++ Assignment 4/Code/ex2.py
@@ -7,7 +7,7 @@ from mpl_toolkits.mplot3d import Axes3D
 '''
 Goal: estimating multinomial gaussian using perceptron
 '''
-def mlp(X, t, eta = 1e-1, gamma = 0, num = 1e3, K = 1, M = 8):
+def mlp(X, t, eta = 1e-1, gamma = 0, num = 1e3, K = 1, M = 8, save = 4):
     '''
     Multi-layered-pereptron
     K = output nodes
@@ -28,14 +28,18 @@ def mlp(X, t, eta = 1e-1, gamma = 0, num = 1e3, K = 1, M = 8):
     # hidden weights and outputweights
     wh = np.random.rand(D, M) - 1/2
     wo = np.random.rand(M, K) - 1/2
-    for i in range(num):
+    errors = np.zeros(num + 1)
+    preds = [[]] * (save + 1)
+    idx = 0
+    tmp = num // save
+    for i in range(num + 1):
         # forward pass
         a = X.dot(wh)
         z = np.tanh(a)
         # compute prediction
-        preds = z.dot(wo)
+        y = z.dot(wo)
         # backward pass
-        dk = t - preds
+        dk = t - y
         # compute hidden activation; note elementwise product!!
         dj = (1 - z**2) * ((wo.dot(dk.T).T))
         # dj = z * (1 - z ) * ((wo.dot(dk.T)).T)
@@ -44,7 +48,8 @@ def mlp(X, t, eta = 1e-1, gamma = 0, num = 1e3, K = 1, M = 8):
         E2 = X.T.dot(dj)
         wo += eta * E1 + gamma * wo
         wh += eta * E2 + gamma * wh
-        error = np.sum((preds-t)**2)
+        error = np.sum((y-t)**2)
+        errors[i] = error
         # print(error)
         if error < 1e-1:
             print('error low enough')
@@ -52,70 +57,91 @@ def mlp(X, t, eta = 1e-1, gamma = 0, num = 1e3, K = 1, M = 8):
         if error == nan:
             print('nan encountered')
             break
+        if  i % tmp == 0:
+            preds[idx] = [y,i]
+            idx += 1
         # print(i)
-    return preds, error
+    return errors, preds
 
+def plot_results(errors, pred, tar, inputs):
+    # plot the end result
+    tmp = int(np.sqrt(inputs.shape[0]))
+    print(inputs.shape)
+    inputs = inputs.reshape(tmp, tmp, inputs.shape[1])
+    x = inputs[:,:,0]; y = inputs[:,:,1]
 
+    fig, ax = subplots(1,1, subplot_kw = {'projection': '3d'})
+    ax.scatter(x, y, preds[-1][0].reshape(x.shape))
+    ax.scatter(x, y, tar.reshape(x.shape))
+    ax.legend(['prediction', 'target'], loc = 0)
+    savefig('../Figures/ex231')
+
+    # plot the training
+    fig, ax = subplots(2,2, subplot_kw = {'projection': '3d'})
+    for idx, axi in enumerate(ax.flatten()):
+        data, cycle = preds[idx]
+        axi.scatter(x, y, data.flatten().reshape(x.shape))
+        axi.set_title('cycle : {0}'.format(cycle))
+        axi.set_xlabel('$x_1$', labelpad = 10)
+        axi.set_ylabel('$x_2$', labelpad = 10)
+        axi.set_zlabel('pdf', labelpad = 10 )
+    fig.tight_layout()
+    savefig('../Figures/232')
+
+    # plot the errors
+    fig, ax = subplots(1,1)
+    ax.plot(errors)
+    ax.set_ylabel('SSE')
+    ax.set_xlabel('Iteration')
+    ax.set_title('Sum squared error as a function of iteration')
+
+    show(0)
+close('all')
 # q1
 # create the target distribution
-tmp = np.arange(-2,2, .1)
+tmp = np.arange(-2,2, .2)
 x, y = np.meshgrid(tmp, tmp)
 
 mu = [0,0]
 sigma = np.eye(len(mu)) * 2/5
 dist = mv(mu, sigma)
 
-X = np.vstack((x.flatten(), y.flatten()))
-# Y =  np.log(dist.pdf(X.T).reshape(x.shape))* 3
-Y = dist.pdf(X.T).reshape(x.shape)  * 3
-# shufIdx = np.random.permutation(range(X.shape[0]))
-# shufX = X[shufIdx,:]
-# shufT = targets[shufIdx]
+X = np.vstack((x.flatten(), y.flatten())).T
+Y = dist.pdf(X).reshape(x.shape)  * 3
 targets = np.array(Y.flatten(), ndmin = 2).T
 
-# dist = mv(0, 2/5)
-# tmp = np.linspace(-1,1, 100)
-# targets = np.array(dist.pdf(tmp), ndmin = 2).T  * 3
-# targets = np.array(np.sin(tmp),ndmin = 2).T
-# targets = np.array(tmp**2,ndmin = 2).T
-# targets = np.array( np.exp(-(tmp)**2), ndmin = 2).T
-# X = np.array(tmp, ndmin = 2).T
+
+errors, preds = mlp(X, targets, eta = 1e-3, num = 1e3)
+# plot_results(errors, preds, targets, X)
+
+#  q 4 : random shuffle index
 idx = np.random.permutation(range(len(targets)))
-X = X.T
-# X = X[idx,:]
-# targets = targets[idx]
-# print(idx)
-preds, error = mlp(X, targets, eta = 1e-3,M = 8, num = 1800)
+shuffleX = X[idx,:]
+shuffleTargets = targets[idx]
+errors_shuff, preds = mlp(shuffleX, shuffleTargets, 1e-3)
+# plot_results(errors, preds, shuffleTargets, shuffleX)
+
+# it is faster sometimes, dunno why;
+# im asuming that it hits the right values such that the constraints get updated
+# farely quickly
+fig, ax = subplots(1,1)
+ax.plot(errors, label = 'original')
+ax.plot(errors_shuff, label = 'shuffled')
+ax.legend(loc = 0)
+
+# q5 : load file, create distribution
+X, Y, target = list(np.loadtxt('../Data/a017_NNpdfGaussMix.txt').T)
+tmp = int(np.sqrt(target.shape[0]))
+x = X.reshape(tmp,tmp)
+y = Y.reshape(tmp, tmp)
+
+X = np.vstack((X,Y)).T
+target = np.array(target, ndmin = 2).T
 fig, ax = subplots(1,1, subplot_kw = {'projection': '3d'})
-# ax.scatter(X, targets)
-# ax.scatter(X, preds)
-# ax.legend(['target','prediction'], loc = 0)
-# ax.set_title('SSE =' + str(error))
-ax.scatter(xs = X[:,0], ys = X[:,1], zs = preds)
-ax.scatter(xs = X[:,0], ys = X[:,1], zs = targets)
+ax.plot_surface(x, y, target.reshape(tmp, tmp), cmap = 'viridis')
 
-ax.legend(['prediction', 'target'], loc = 0)
-
-fig, ax = subplots(1,1, subplot_kw = {'projection': '3d'})
-ax.plot_surface(X[:,0].reshape(x.shape), X[:,1].reshape(x.shape), preds.flatten().reshape(x.shape))
-show()
-
-# # test case: this works
-# X = np.array([[0,0],[0,1],[1,0],[1,1]])
-# targets = np.array([0,1,1,0], ndmin =2 ).T
-# pred, error = mlp(X, targets, eta = .1)
-# print(error)
-
-
-close('all')
-# fig, ax = subplots(1, 1, subplot_kw = {'projection': '3d'})
-# x, y = list(X.T)
-#
-# tmp = np.argsort(X,0)
-# tmpx, tmpy = tmp[:,0], tmp[:,1]
-# d = int(np.sqrt(x.shape[0]))
-# shape = (d,d)
-# # x, y, preds = x[tmpx].reshape(shape), y[tmpy].reshape(shape),  preds[tmpx].flatten().reshape(shape)
-# ax.scatter(xs = x, ys= y, zs = preds)
-# ax.scatter(xs = x, ys = y, zs = targets)
-# show(0)
+# q6 train the network on the dataset
+errors, preds = mlp(X, target, eta = 1e-8,num = 1e5,  M = 400)
+fig, ax = subplots()
+ax.plot(errors)
+show(0)
